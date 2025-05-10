e8f0c4f86b5acbfc73b0b33b3e97365cc500d45b
Abdullah Khan <abdullahjan2780@gmail.com>
2025-05-10 20:07:43 +0500

    advanced js

---
+++ advancedjs/appM.js
@@ -0,0 +1,7 @@
+import { multiply } from "./mathOperationM.js";
+import { add } from "./mathOperationM.js";
+import { subtract } from "./mathOperationM.js";
+
+console.log(multiply(2, 3));
+console.log(add(2, 3));
+console.log(subtract(2, 3));
